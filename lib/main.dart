79fdf5e35b22f3da9fe56a34f12d8be67578eb0e
Psicko <alvinnothing15@gmail.com>
2023-10-05 16:43:25 +0700

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,29 @@ import 'package:flutter/gestures.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/utils.dart';
-import 'package:myapp/page/logo.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+// import 'package:myapp/page/logo.dart';
 // import 'package:myapp/page/homepage.dart';
 // import 'package:myapp/page/schedule.dart';
-// import 'package:myapp/page/loginpage.dart';
+import 'package:myapp/page/loginpage.dart';
 // import 'package:myapp/page/about.dart';
 // import 'package:myapp/page/rgb.dart';
 // import 'package:myapp/page/regispage.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
 	return MaterialApp(
-		title: 'Flutter',
+		title: 'SmartAquarium',
 		debugShowCheckedModeBanner: false,
 		scrollBehavior: MyCustomScrollBehavior(),
 		theme: ThemeData(
